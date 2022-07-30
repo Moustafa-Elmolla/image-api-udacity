e27cb86b19162c42d20f5b96de0ef51af63ee58d
Moustafa-Elmolla <elmolla1988@hotmail.com>
2022-07-30 19:47:32 +0200

    no more

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -7,14 +7,12 @@ const request = supertest(app);
 describe('Test endpoint response', () => {
 
     it('gets the api endpoint ', async ():Promise<void> => {
-        // jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
         const response: supertest.Response = await request.get(
             '/api/images?filename=palmtunnel.jpg&width=100&height=100'
         );
         expect(response.status).toBe(200);
     });
     it('should be error, Return 404', async ():Promise<void> => {
-        // jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
         const response: supertest.Response = await request.get(
             '/api/images?filename=palmtunnel.jpg&width=100&height=100'
         );
